eef95639ca0b0ceacf144f8dbae09f863f17af0e
Mohammad Waris <mdtech9900@gmail.com>
2026-06-23 17:05:49 +0530

    two sum prblem

--- array.js
+++ array.js
@@ -4,15 +4,15 @@
 const nums=[2,3,5,1,7,8]
 const target = 10
 
-// const twoSum = (nums,target)=>{ //O(n2)
-//   for(let i=0;i<nums.length;i++){
-//     for(let j=i+1;j<nums.length;j++){
-//         if(nums[i]+nums[j]===target){
-//             return [i,j]
-//         }
-//     }
-//   }
-// }
+const twoSum = (nums,target)=>{ //O(n2)
+  for(let i=0;i<nums.length;i++){
+    for(let j=i+1;j<nums.length;j++){
+        if(nums[i]+nums[j]===target){
+            return [i,j]
+        }
+    }
+  }
+}
 
 
 const twoSum=(nums,target)=>{
